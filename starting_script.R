09f52f4ec95e5cf1938f6b94f1f2f90b239f421d
Minh-AnhHuynh <minh-anh.huynh@etu.u-paris.fr>
2022-08-24 18:54:37 +0200

    Addd rss feed

--- starting_script.R
+++ starting_script.R
@@ -1,4 +1,10 @@
+# First commands to start a blog
 usethis::use_git()
 usethis::use_github()
 shell("git push")
 
+# Commands to git commit
+shell("git commit")
+
+# Link of the website
+https://minh-anhhuynh.github.io/Project-Equilibrium/
