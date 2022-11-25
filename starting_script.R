e970c966075222a5210d21697f5918095eb44a52
Minh-Anh Huynh Ideapad <minh-anh.huynh@etu.u-paris.fr>
2022-11-26 00:29:14 +0100

    Add "How to add only part of figure caption" post and "process of writing"

--- starting_script.R
+++ starting_script.R
@@ -3,11 +3,10 @@ usethis::use_git()
 usethis::use_github()
 shell("git push")
 
-# Commands to git commit
-shell("git commit")
 
-<<<<<<< HEAD
+
 # Link of the website
-https://minh-anhhuynh.github.io/Project-Equilibrium/
-=======
->>>>>>> ec72730218cdf0f3b87e2e13199b9d94359ddd88
+# https://minh-anhhuynh.github.io/Project-Equilibrium/
+
+# Link of Github
+# https://github.com/Minh-AnhHuynh/Project-Equilibrium/settings/pages
